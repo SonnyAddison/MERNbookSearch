e377beb03f6e8e8f10f076cbad7ea961a11162c7
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-10-04 16:00:51 -0700

    Building out files to work

---
+++ client/src/utils/queries.js
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client';
+
+
+
+
+export const QUERY_ME = gql`
+    queary me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const searchGoogleBooks = (query) => {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+};
+
