263e481b841a05157d0758a5b65715dd830951a7
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-10-16 10:53:25 -0700

    working on getting heroku fixed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks' && 'mongodb://localhost:27017/your-database-name', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks' && 'mongodb://localhost:27017/BOOK_SEARCHDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
